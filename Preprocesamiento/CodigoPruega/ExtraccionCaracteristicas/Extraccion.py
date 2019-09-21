a8453904fc38812442e49dc256820ca35a6b34b8
AndresHGdeG <carlos-andres-hg@hotmail.com>
2019-09-21 14:15:09 -0500

    Se ha agregado las noticias de prueba por cada seccion(70 noticias) en el directorio NoticiasPrueba, a demas se ha integrado todo en un mismo documento llamado UnionPrueba.txt. El archivo Entrenamiento/Extraccion.py se ha complementado para hacer pruebas con el algoritmo naive bayer con 325  noticias de entrenamiento y 25 de prueba

--- Preprocesamiento/CodigoPruega/ExtraccionCaracteristicas/Extraccion.py
+++ Preprocesamiento/CodigoPruega/ExtraccionCaracteristicas/Extraccion.py
@@ -7,22 +7,23 @@ import pickle
 
 
 
-data=pd.read_csv('../../NoticiasPrueba/pruebaNoticias.csv')
-noticias=data['noticia'][0:6]
-
+data=pd.read_csv('../../../Corpus/Noticias/aux.txt', sep='&&&&&',engine='python')
+noticias=data['noticia']
+#print (data)
+print(noticias)
 vectorizer=CountVectorizer()
 X=vectorizer.fit_transform(noticias)
-#print(vectorizer.get_feature_names())
+#	#print(vectorizer.get_feature_names())
 caracteristicas=str(vectorizer.get_feature_names());
-#print(caracteristicas)
+#	#print(caracteristicas)
 fCaracteristicas=open('caracteristicas.txt','w')
 fCaracteristicas.write(caracteristicas)
 fCaracteristicas.close()
-
+#
 #
 f=open('vector.txt','w')
 x=X.toarray()
-
+#
 for item in x:
 	for num in item: 
 		numT=str(num)
@@ -30,12 +31,12 @@ for item in x:
 		f.write(" ")
 
 	f.write("\n\n")
-	
-	
-
-
+#	
+#	
+#
+#
 f.close()
-
+#
 print(" se ha creado 2 archivos, 1 con las caracteristicas y otro con los vectores")
-#print(X.toarray())
+
 
