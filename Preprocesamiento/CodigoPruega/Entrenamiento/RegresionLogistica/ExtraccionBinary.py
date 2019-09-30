d09a74fa38d37a05702565040c1711b70983488b
AndresHGdeG <carlos-andres-hg@hotmail.com>
2019-09-30 17:45:08 -0500

    Se han creado las primeras pruebas con validación  cruzada y los resultados con preccición y la matriz de confución en el algoritmo regresión logística en la extracción de caracteristicasd binaria

--- Preprocesamiento/CodigoPruega/Entrenamiento/RegresionLogistica/ExtraccionBinary.py
+++ Preprocesamiento/CodigoPruega/Entrenamiento/RegresionLogistica/ExtraccionBinary.py
@@ -1,13 +1,15 @@
-from __future__ import division #esta libreria nos ayuda a incluir punto decimal en las diviciones enteras
 
 from sklearn.feature_extraction.text import CountVectorizer#esta libreria nos permite extraer las caracteristicas de las noticias formando un espacio vectorial por cada una 
  
 import pandas as pd #esta libreria nos permite extraer datos de archivos csv
 
 from sklearn.linear_model import LogisticRegression #esta libreria nos permite entrenar el algoritmo Regresion logistica
-
-
 data=pd.read_csv('../../../NoticiasPrueba/UnionPrueba.txt', sep='&&&&&',engine='python') #se cargan los datos de archivo csv
+
+from sklearn.model_selection import KFold # permite generar la validacion cruzada, dividiendo el conjunto de entrenamiento y conjunto de prueba
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix#Matriz de confucion
+
 
 noticias=data['noticia']# se extrae todas las noticias de la columna noticia
 seccion=data['seccion']# se extraen las secciones correspondientes a cada noticia
@@ -15,38 +17,36 @@ seccion=data['seccion']# se extraen las secciones correspondientes a cada notici
 #print (data)
 #print(noticias)
 
-vectorizer=CountVectorizer(binary=1)
-X=vectorizer.fit_transform(noticias[0:324])# se extraer las caracteristicas de 325 noticias 
-Y=seccion[0:324]#Se extraen las secciones de las primeras 325 noticias
 
-Y_prueba=data.loc[325:350,'seccion']# secciones correspondientes a la prueba
-X_prueba=vectorizer.transform(noticias[325:350])# se extraen las caracteristicas de las noticias con las cuales se va a probar el modelo 
+vectorizer=CountVectorizer(binary=1)
+X=vectorizer.fit_transform(noticias)# se extraer las caracteristicas de las noticias 
+Y=seccion #Se extraen las secciones correspondientes a cada noticia
+
+
+clf=LogisticRegression(random_state=1,solver='sag',multi_class='multinomial',max_iter=500)
+
+
+Kf=KFold(n_splits=2,random_state=1,shuffle=True)#Este metodo crea una instancia para generar la validacion  cruzada
+#print(Kf)
+for train_index,test_index in Kf.split(X):
+	X_train,X_test=X[train_index],X[test_index]
+	Y_train,Y_test=Y[train_index],Y[train_index]
+	
+	clf.fit(X_train,Y_train) #se crea el modelo del algoritmo, donde X es el espacio vectorial de las noticias de entrenamiento, y Y es la secciona la cula pertenece cada espacio vectoria
+	
+	X_resultado=clf.predict(X_test)
+
+	accuP=accuracy_score(X_resultado,Y_test) # Se calcula la precicion
+	accuN=accuracy_score(X_resultado,Y_test,normalize=False) # Se calcula la precicion
+	print("precicion %: ",accuP),
+	print("numero correctas:",accuN,"de: ",len(X_resultado))
+
+	matrizConfusion=confusion_matrix(X_resultado,Y_test, labels=[0,1,2,3,4])
+	print(matrizConfusion)
 
 #0-Deportes
 #1-Economia
 #2-Politica
-#3-Culutura
+#3-Cultura
 #4-Ciencia y tecnologia
-
-
-clf=LogisticRegression(random_state=0,solver='sag',multi_class='multinomial',max_iter=500)
-clf.fit(X,Y) #se crea el modelo del algoritmo, donde X es el espacio vectorial de las noticias de entrenamiento, y Y es la secciona la cula pertenece cada espacio vectorial
-
-X_resultado=clf.predict(X_prueba)
-#print (X_resultado) #Se precide las noticias de prueba
-contador=0
-numNoticias=25
-
-for i in range(numNoticias):
-
- if Y_prueba[325+i]==X_resultado[i]:
-  contador=contador+1
-# print X_resultado[i],
-# print Y_prueba[325+i],
-
-exactitud=100*contador/numNoticias
-print("Exactitud",exactitud)
-
-
-#print( data[325:350])# se muestran las nooticias de prueba
 
