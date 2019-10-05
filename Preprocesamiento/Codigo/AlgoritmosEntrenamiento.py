eeeea2c0c16449a889d0c4cfc196a3f58823228a
AndresHGdeG <carlos-andres-hg@hotmail.com>
2019-10-05 15:21:26 -0500

    Se ha modificado el archivo Preprocesamiento/Codigo/AlgoritmosEntrenamiento.py para que registre el tiempo ocupado por el procedo de validación cruzada tanto user,sys y real, en el directorio Preprocesamiento/Codigo se ha creado un Makefile para crear las pruebas de forma automática, escribiendo los resultados en archivos dentro del mismo directorio, además se ha creado le Directorio Resultados donde se han almacenado los resultados de las pruebas hechas por cada algorimto y en combinación de la selección de caracteristicas de forma binaria y en frecuencia

--- Preprocesamiento/Codigo/AlgoritmosEntrenamiento.py
+++ Preprocesamiento/Codigo/AlgoritmosEntrenamiento.py
@@ -1,8 +1,11 @@
+from __future__ import division# permite divir numeros enteros y regrear flotanes
 import sys# Libreria para recibir los argumentos de la linea de comandos
 import time #Esta libreria permitira medir el timepo de ejecucion de los algoritmos
+from resource import getrusage as resource_usage,RUSAGE_SELF# permite obtener el tiempo ocupado por el proceso en el sistema
 import platform# permite obtener los datos de la computdora
 import psutil# permite extraer el numero de CPUs
 import datetime# Se obtemdra el dia en que las pruebas se hagan
+import os # permite conocer la memoria consumida por el programa
 
 from sklearn.feature_extraction.text import CountVectorizer#esta libreria nos permite extraer las caracteristicas de las noticias formando un espacio vectorial por cada una  
 import pandas as pd #esta libreria nos permite extraer datos de archivos csv
@@ -36,17 +39,21 @@ def importarAlgoritmo(algoritmo):
 	
 	return clf
 
+def get_process_memory():
+	process=psutil.Process(os.getpid())
+	return process.memory_info().rss/1000000 #Megabytes
+
+
 #--------------------------------------MAIN()----------------------------------#
 
-if len(sys.argv)<2:
-	print "Se debe incluir <NombreAlgoritmo> <RutaNoticias> enla linea de comandos"
+if len(sys.argv)<3:
+	print "Se debe incluir <NombreAlgoritmo> <RutaNoticias> <Modo Binario(1)/Frecuencia(0)> enla linea de comandos"
 	sys.exit(2)
 
 algoritmo=sys.argv[1]# Es el nombre del algoritmo, NaiveBayes, RegresionLogistica,RandomForest y MSV
 ventanas=int(sys.argv[2]) #Es el numero de diviciones en la validacion cruzada
 ruta=sys.argv[3] #Es la ruta del corpus
-
-modoBinario=1
+modoBinario=sys.argv[4]
 
 noticiasTotales=0
 noticiasCorrectas=0
@@ -54,7 +61,7 @@ accuracyPromedio=0
 fmeasurePromedio=0
 recallPromedio=0
 precisionPromedio=0
-
+matrizConfusionPromedio=0
 
 
 clf=importarAlgoritmo(algoritmo)#Se crea la instacia CLF quien genera el modelo del algoritmo
@@ -76,8 +83,9 @@ Y=seccion #Se extraen las secciones correspondientes a cada noticia
 Kf=KFold(n_splits=ventanas,random_state=1,shuffle=True)#Este metodo crea una instancia para generar la validacion  cruzada
 #print(Kf)
 
+memoriaInicial=get_process_memory()
+start_time,start_resource=time.time(),resource_usage(RUSAGE_SELF)
 
-tiempoInicio=time.time()# inicio del tiempo
 
 for train_index,test_index in Kf.split(X):
 	X_train,X_test=X[train_index],X[test_index]
@@ -109,43 +117,69 @@ for train_index,test_index in Kf.split(X):
 	#print("Precision:",precision)
 	
 	matrizConfusion=confusion_matrix(X_resultado,Y_test, labels=[0,1,2,3,4])
+	matrizConfusionPromedio+=matrizConfusion
 	#print(matrizConfusion)
 
 	#print("")
-tiempoFinal=time.time()
 
+end_resource,end_time=resource_usage(RUSAGE_SELF),time.time()
+memoriaFinal=get_process_memory()
+
+# print 'real',end_time-start_time
+# print 'user',end_resource.ru_utime-start_resource.ru_utime
+# print 'sys',end_resource.ru_stime-start_resource.ru_stime
+
+
+matrizConfusionPromedio=matrizConfusionPromedio/ventanas
 accuracyPromedio/=ventanas
 fmeasurePromedio/=ventanas
 recallPromedio/=ventanas
 precisionPromedio/=ventanas
 
-print "tiempo de ejecucion",tiempoFinal-tiempoInicio," segundos"
-	
-print "Accuracy Promedio:",accuracyPromedio
-print "Fmeasure promedio:",fmeasurePromedio
-print "Recall promedio:",recallPromedio
-print "precision promedio:",precisionPromedio
+# print "Matriz confusion promedio: ",matrizConfusionPromedio
+# print "Memoria usada: ",memoriaFinal-memoriaInicial,"  MB"
+# print "Accuracy Promedio:",accuracyPromedio
+# print "Fmeasure promedio:",fmeasurePromedio
+# print "Recall promedio:",recallPromedio
+# print "precision promedio:",precisionPromedio
 
-nombreArchivo="Resul_"+algoritmo+"_"+str(ventanas)+".txt"
+nombreArchivo=algoritmo+"_"+"Div("+str(ventanas)+")_SelCaract("+str(modoBinario)+").txt"
 resultados=open(nombreArchivo, "w+")
 resultados.write("Algoritmo:"+algoritmo)
 resultados.write("\nNumero de ventanas:"+str(ventanas))
+resultados.write("\nNoticias de entrenamiento: "+str(len(train_index)))
+resultados.write("\nNoticias de test: "+str(len(test_index)))
 
 if modoBinario:
 	resultados.write("\nseleccion de caracteristicas: Modo binario")
 else:
 	resultados.write("\nseleccion de caracteristicas: Por frecuencia")
 
-resultados.write("\nTiempo de ejecucion:"+str(tiempoFinal-tiempoInicio))
-resultados.write("\n\nClasificadas correctamente:"+str(noticiasCorrectas)+" de:"+str(noticiasTotales))
+resultados.write("\n\nNoticias clasificadas correctamente:"+str(noticiasCorrectas)+" de:"+str(noticiasTotales))
 resultados.write("\nAccuracy promedio:"+str(accuracyPromedio))
 resultados.write("\nFmeasure promedio:"+str(fmeasurePromedio))
 resultados.write("\nRecall promedio:"+str(recallPromedio))
 resultados.write("\nPrecision promedio:"+str(precisionPromedio))
-resultados.write("\n\nMatriz de confucion\n")
-resultados.write(str(matrizConfusion))
+resultados.write("\n\nMatriz de confucion promedio\n")
+
+resultados.write(str(matrizConfusionPromedio.round()))
 resultados.write("\n\nOrden de etiquetas:\n 0 1 2 3 4")
 resultados.write("\nDonde \n0:Deportes\n1:Economia\n2:Politica\n3:Cultura\n4:Ciencia y tecnologia")
+
+resultados.write("\n\nCorpus ocupado para validacion cruzada")
+resultados.write("\n"+ruta)
+
+resultados.write("\n\nInformacion de cada division en la validacion cruzada")
+resultados.write("\nMemoria ocupada(Megabytes):"+str((memoriaFinal-memoriaInicial)/ventanas)+" MB")
+resultados.write("\nTiempo real de ejecucion(segundos):"+str((end_time-start_time)/ventanas)+" s")
+resultados.write("\nTiempo ocupado por el usuario(segundos):"+str((end_resource.ru_utime-start_resource.ru_utime)/ventanas)+" s")
+resultados.write("\nTiempo promedio ocupado en el sistema/CPU (segundos):"+str((end_resource.ru_stime-start_resource.ru_stime)/ventanas)+" s")
+
+resultados.write("\n\nInformacion total del proceso de validacion cruzada")
+resultados.write("\nMemoria ocupada(Megabytes):"+str(memoriaFinal-memoriaInicial)+" MB")
+resultados.write("\nTiempo real de ejecucion(segundos):"+str(end_time-start_time)+" s")
+resultados.write("\nTiempo ocupado por el usuario(segundos):"+str(end_resource.ru_utime-start_resource.ru_utime)+" s")
+resultados.write("\nTiempo ocupado en el sistema/CPU (segundos):"+str(end_resource.ru_stime-start_resource.ru_stime)+" s")
 
 resultados.write("\n\nInformacion del equipo")
 resultados.write("\nSystem OS:"+platform.system())
